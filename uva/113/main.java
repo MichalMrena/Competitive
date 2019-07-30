efdaff071954f8face9bc296d5fa1f610f321d54
Michal Mrena <mrena.michal@gmail.com>
2019-07-30 20:09:13 +0200

    Here it is.

---
+++ uva/113/main.java
@@ -0,0 +1,45 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    static int N;
+    static BigInteger P;
+
+    static BigInteger diff(int k) {
+        BigInteger pow = BigInteger.valueOf(k).pow(N);
+        return pow.subtract(P).abs();
+    }
+
+    static int searchForK(int left, int right) {
+        int center = left + (right - left) / 2;
+
+        BigInteger diffCenter = diff(center);
+
+        if (diffCenter.equals(BigInteger.ZERO)) {
+            return center;
+        }
+
+        BigInteger diffLeft = diff(center - 1);
+
+        if (diffCenter.compareTo(diffLeft) < 0) {
+            return searchForK(center + 1, right);
+        } else {
+            return searchForK(left, center - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            N = sc.nextInt();
+            P = new BigInteger(sc.next());
+
+            System.out.println(searchForK(1, 1_000_000_000));
+        }
+
+        sc.close();
+    }
+
+}
---
+++ uva/113/notes.md
@@ -0,0 +1,3 @@
+# UVA 113 Power of Cryptography
+[problem description](https://uva.onlinejudge.org/external/1/113.pdf)
+I have picked Java for this problem because you need to deal with big numbers and Java has ```BigInteger``` in its standard library.
